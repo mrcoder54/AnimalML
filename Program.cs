7d2475d75683c55fcb5c9721d6cb57c33df8720a
Marco Rodriguez <mrcoder54@gmail.com>
2023-11-17 11:18:33 -0500

    Inital project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MLTest;
+
+Console.WriteLine("Hello, World!");
+
+
+//Load sample data
+var imageBytes = File.ReadAllBytes(@"puppy.jpg");
+PhotoML.ModelInput sampleData = new PhotoML.ModelInput()
+{
+    ImageSource = imageBytes,
+};
+
+//Load model and predict output
+var result = PhotoML.Predict(sampleData);
+
+Console.WriteLine(result.PredictedLabel);
+Console.ReadLine();
